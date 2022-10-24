b9a8bc403e64c9b6e0beb31d4a86dee5b893c62f
Vladislav Vyalov <vyalov92@yandex.ru>
2022-10-24 15:25:57 +0300

    main

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,20 @@
-const router = require('express').Router();
+const userRouter = require('express').Router();
 const { celebrate, Joi } = require('celebrate');
 const {
-  getCurrentUser, updateUser
+  getUser,
+  updateUser,
 } = require('../controllers/users');
 
-router.get('/me', getCurrentUser);
-router.patch('/me',
+userRouter.get('/me', getUser);
+userRouter.patch(
+  '/me',
   celebrate({
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30),
-      email: Joi.string().email()
-    })
+      email: Joi.string().email(),
+    }),
   }),
-  updateUser);
+  updateUser,
+);
 
-
-module.exports = router;
+module.exports = userRouter;
