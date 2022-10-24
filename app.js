b268a4562316117ad520a0b815c54e3713f76fc0
Vladislav Vyalov <vyalov92@yandex.ru>
2022-10-24 16:26:37 +0300

    main

--- app.js
+++ app.js
@@ -28,11 +28,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  const { origin } = req.headers;
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
   const requestHeaders = req.headers['access-control-request-headers'];
-
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
