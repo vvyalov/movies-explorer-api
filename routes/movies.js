4843bcc03e49f3244a71e9cd02bdaa0e25be30c9
Vladislav Vyalov <vyalov92@yandex.ru>
2022-10-24 11:12:47 +0300

    level-1

--- routes/movies.js
+++ routes/movies.js
@@ -1,56 +1,36 @@
-const movieRouter = require('express').Router();
+const router = require('express').Router();
 const { isObjectIdOrHexString } = require('mongoose');
-const { isURL } = require('validator');
 const { celebrate, Joi } = require('celebrate');
-const { getMovies, createMovie, deleteMovie } = require('../controllers/movies');
+const {
+  newMovie, deleteMovie, getMovie
+} = require('../controllers/movies');
 
-const validationId = (value) => {
+const validate = (value) => {
   if (isObjectIdOrHexString(value)) {
     return value;
   }
-  throw new Error('Передан некорректный id фильма');
+  throw new Error('Некорректный _id карточки');
 };
 
-const validationUrl = (value) => {
-  if (isURL(value)) {
-    return value;
-  }
-  throw new Error('Передана некорректная ссылка');
-};
-
-// возвращает все сохранённые текущим пользователем фильмы
-movieRouter.get('/', getMovies);
-
-// создаёт фильм
-movieRouter.post(
-  '/',
+router.get('/', getMovie);
+router.post('/',
   celebrate({
     body: Joi.object().keys({
-      country: Joi.string().required(),
-      director: Joi.string().required(),
-      duration: Joi.number().required(),
-      year: Joi.string().required(),
-      description: Joi.string().required(),
-      image: Joi.string().required().custom(validationUrl),
-      trailerLink: Joi.string().required().custom(validationUrl),
-      nameRU: Joi.string().required(),
-      nameEN: Joi.string().required(),
-      thumbnail: Joi.string().required().custom(validationUrl),
-      movieId: Joi.number().required(), // id из ответа стороннего api
-    }),
+      country: Joi.string().required,
+      director: Joi.string().required,
+      duration: Joi.number().required,
+      year: Joi.string().required,
+      description: Joi.string().required,
+      image: Joi.string().required,
+      trailerLink: Joi.string().required,
+      thumbnail: Joi.string().required,
+      movieId: Joi.number().required,
+      nameRU: Joi.string().required,
+      nameEN: Joi.string().required,
+    })
   }),
-  createMovie,
-);
+  newMovie);
+router.delete('/_id', deleteMovie);
 
-// удаляет сохранённый фильм по id
-movieRouter.delete(
-  '/:movieDeleteId',
-  celebrate({
-    params: Joi.object().keys({
-      movieDeleteId: Joi.string().required().custom(validationId),
-    }),
-  }),
-  deleteMovie,
-);
 
-module.exports = movieRouter;
+module.exports = router;
