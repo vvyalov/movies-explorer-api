11be2aa749613f2d93c83295dc3835d092f3e0ce
Vladislav Vyalov <vyalov92@yandex.ru>
2022-10-24 15:01:47 +0300

    main

--- controllers/users.js
+++ controllers/users.js
@@ -13,16 +13,34 @@ const CREATED = 201;
 
 
 const newUser = (req, res, next) => {
-  const userCurrentId = req.user._id;
-  res.header('Access-Control-Allow-Origin', req.headers.origin);
-  res.header('Access-Control-Allow-Credentials', true);
-
-  User.findById(userCurrentId)
+  const { name, email } = req.body;
+  User.findOne({ email })
     .then((user) => {
-      if (!user) {
-        throw new NotFoundError('Пользователь с указанным _id не найден');
+      if (user) {
+        return next(new EmailError('Пользователь с таким email уже существует'));
       }
-      res.send(user);
+      return bcrypt.hash(req.body.password, 10);
+    })
+    .then((hash) => User.create({
+      email, password: hash, name
+    }))
+    .then((user) => {
+      res.status(201).send({
+        name: user.name,
+        about: user.about,
+        _id: user._id,
+      });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new RequestError('Данные заполнены с ошибкой'));
+        return;
+      }
+      if (err.code === 11000) {
+        next(new EmailError('Пользователь с таким email уже существует'));
+        return;
+      }
+      next(err);
     })
     .catch(next);
 };
@@ -32,21 +50,17 @@ const updateUser = (req, res, next) => {
   User.findByIdAndUpdate(req.user._id, { name, email }, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
-        throw new NotFoundError('Пользователь с указанным _id не найден');
+        throw new NotFoundError('Указанный _id не найден');
       }
       res.send(user);
     })
     .catch((err) => {
-      if (err.code === 11000) {
-        next(new ConflictError('Пользователь с таким email уже существует'));
-        return;
-      }
       if (err.name === 'ValidationError') {
-        next(new BadRequestError('Переданы некорректные данные при обновлении профиля'));
+        next(new RequestError('Переданы некорректные данные'));
         return;
       }
       if (err.name === 'CastError') {
-        next(new NotFoundError('Пользователь с указанным _id не найден'));
+        next(new NotFoundError('Указанный _id не найден'));
         return;
       }
       next(err);
@@ -60,46 +74,28 @@ const login = (req, res, next) => {
 
   User.findUserByCredentials({ email, password })
     .then((user) => {
-      const token = jwt.sign({ _id: user._id.toHexString() }, JWT_SECRET, { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id.toHexString }, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key', { expiresIn: '7d' });
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        sameSite: 'none',
-      }).json({ email: user.email });
+        sameSite: true,
+      });
+      res.status(200).send({ token })
     })
     .catch(next);
 };
 
 const getCurrentUser = (req, res, next) => {
-  const {
-    name,
-    email,
-    password,
-  } = req.body;
-  bcrypt.hash(password, SALT_ROUNDS)
-    .then((hash) => {
-      User.create({
-        name,
-        email,
-        password: hash,
-      })
-        .then((user) => {
-          res.status(CREATED).send({
-            name: user.name,
-            email: user.email,
-          });
-        })
-        .catch((err) => {
-          if (err.name === 'ValidationError') {
-            next(new BadRequestError('Данные заполнены с ошибкой или не переданы'));
-            return;
-          }
-          if (err.code === 11000) {
-            next(new ConflictError('Пользователь с таким email уже существует'));
-            return;
-          }
-          next(err);
-        });
+  const userCurrentId = req.user._id;
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Credentials', true);
+
+  User.findById(userCurrentId)
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Указанный _id не найден');
+      }
+      res.send(user);
     })
     .catch(next);
 };
