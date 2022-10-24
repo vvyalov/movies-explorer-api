de0ec6ceb902ed8f4ce9eeaea7006508b17229b2
Vladislav Vyalov <vyalov92@yandex.ru>
2022-10-24 12:21:50 +0300

    main

--- errors/request-error.js
+++ errors/request-error.js
@@ -1,8 +1,8 @@
-class BadRequestError extends Error {
+class RequestError extends Error {
   constructor(message) {
     super(message);
     this.statusCode = 400;
   }
 }
 
-module.exports = BadRequestError;
+module.exports = RequestError;
