b6f16758aae9a5b0c066d0c6c0f7cad8b9aa9318
Vladislav Vyalov <vyalov92@yandex.ru>
2022-10-24 20:12:37 +0300

    main

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.use((req, res, next) => {
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
   const requestHeaders = req.headers['access-control-request-headers'];
-
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
