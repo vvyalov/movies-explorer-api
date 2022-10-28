b1a651b7e7aafefa359102555b101072d42ae7b9
Vladislav Vyalov <vyalov92@yandex.ru>
2022-10-28 14:08:23 +0300

    main

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const bodyParser = require('body-parser');
-const { errors } = require('celebrate');
 require('dotenv').config();
-const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const UserRouter = require('./routes/users');
-const MovieRouter = require('./routes/movies');
-const LoginRouter = require('./routes/login')
-const rateLimit = require('./middlewares/rateLimit');
-const { requestLogger, errorLogger } = require('./middlewares/logger')
+const { errors } = require('celebrate');
+const helmet = require('helmet');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const NotFoundError = require('./errors/not-found-err');
+const CentralizedError = require('./errors/centralized-error');
+const router = require('./routes/index');
 const auth = require('./middlewares/auth');
-const NotFoundError = require('./errors/not-found-error');
-const allowedCors = require('./middlewares/allowedCors');
+const autorizationRouter = require('./routes/autorization');
+const limiter = require('./middlewares/rateLimiter');
 
-const { PORT = 3000, MONGO_DB = 'mongodb://localhost:27017/moviedb' } = process.env;
+const { PORT = 3001, MONGO_DB = 'mongodb://localhost:27017/moviesdb' } = process.env;
 const app = express();
+
+const allowedCors = [
+  'https://vyalov.movie.nomorepartiesxyz.ru',
+  'http://vyalov.movie.nomorepartiesxyz.ru',
+  'http://localhost:3000',
+];
+
+app.use(helmet());
 
 mongoose.connect(MONGO_DB, {
   useNewUrlParser: true,
@@ -23,14 +30,19 @@ mongoose.connect(MONGO_DB, {
 });
 
 app.use(cookieParser());
-app.use(cors(allowedCors));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(requestLogger);
 
 app.use((req, res, next) => {
+  const { origin } = req.headers;
   const { method } = req;
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
   const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
@@ -39,26 +51,18 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(requestLogger);
-app.use(rateLimit);
+app.use(limiter);
 
-app.use('/', LoginRouter)
+app.use(autorizationRouter);
 
-app.use(auth);
-app.use('/users', UserRouter);
-app.use('/movies', MovieRouter);
-
-app.use(errorLogger)
-
-app.use(errors());
+app.use(auth, router);
 
 app.use((req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send({ message: statusCode === 500 ? 'На сервере произошла ошибка' : message });
-  next();
-});
+
+app.use(errorLogger);
+app.use(errors());
+app.use(CentralizedError);
 
 app.listen(PORT);
