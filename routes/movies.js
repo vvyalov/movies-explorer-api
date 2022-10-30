1ea82552f5ef6b14b3025700e6d543f2f19fee7a
Vladislav Vyalov <vyalov92@yandex.ru>
2022-10-30 21:32:39 +0300

    main

--- routes/movies.js
+++ routes/movies.js
@@ -16,17 +16,17 @@ router.get('/', getMovie);
 router.post('/',
   celebrate({
     body: Joi.object().keys({
-      country: Joi.string().required,
-      director: Joi.string().required,
-      duration: Joi.number().required,
-      year: Joi.string().required,
-      description: Joi.string().required,
-      image: Joi.string().required,
-      trailerLink: Joi.string().required,
-      thumbnail: Joi.string().required,
-      movieId: Joi.number().required,
-      nameRU: Joi.string().required,
-      nameEN: Joi.string().required,
+      country: Joi.string().required(),
+      director: Joi.string().required(),
+      duration: Joi.number().required(),
+      year: Joi.string().required(),
+      description: Joi.string().required(),
+      image: Joi.string().required().custom(validate),
+      trailerLink: Joi.string().required().custom(validate),
+      thumbnail: Joi.string().required().custom(validate),
+      movieId: Joi.number().required(),
+      nameRU: Joi.string().required(),
+      nameEN: Joi.string().required(),
     })
   }),
   newMovie);
